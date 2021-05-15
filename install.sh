d9e4637ce285d88e4c3e84b3baa4141a0f2b3e41
Zaedus <nintendozaedus@gmail.com>
2021-05-15 13:36:35 -0500

    Fixed stdin issue

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ AUR_HELPER="yay"
 APP_DEPS="dunst alacritty nautilus"
 BACKEND_DEPS="picom-jonaburg-git acpi pacman-contrib checkupdates-aur alsa-utils"
 UTIL_DEPS="nitrogen polkit-gnome"
-BUILD_DEPS="gtk2 gtk3 libx11 libxinerama libxft-bgra freetype2"
+BUILD_DEPS="gtk2 gtk3 libx11 libxinerama libxft-bgra freetype2 git make"
 THEME_DEPS="arc-icon-theme arc-gtk-theme"
 
 
@@ -46,7 +46,7 @@ install_dotfiles() {
 
 backup_dotfiles() {
 	echo "This is going to overwrite your alacritty, dunst, gtk-2.0, gtk-3.0, and picom configs and the '~/.dwm' directory."
-	read -p "Are you sure you want to continue? [y/N] " -n 1 -r
+	read -p "Are you sure you want to continue? [y/N] " -n 1 -r REPLY </dev/tty
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		return 0
 	else

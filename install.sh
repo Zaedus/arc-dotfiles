ff28feeb02cfa42794e0fba69b63aef8ca03e777
Zaedus <nintendozaedus@gmail.com>
2021-05-15 14:56:01 -0500

    Added final setup and font dependencies

--- install.sh
+++ install.sh
@@ -10,8 +10,18 @@ APP_DEPS="dunst alacritty nautilus"
 BACKEND_DEPS="picom-jonaburg-git acpi pacman-contrib checkupdates-aur alsa-utils"
 UTIL_DEPS="nitrogen polkit-gnome"
 BUILD_DEPS="gtk2 gtk3 libx11 libxinerama libxft-bgra freetype2 git make"
-THEME_DEPS="arc-icon-theme arc-gtk-theme"
+THEME_DEPS="arc-icon-theme arc-gtk-theme nerd-fonts-hack otf-font-awesome-5-free"
 
+final_setup() {
+	cd /tmp
+
+	if [ ! -d /tmp/arc-dotfiles ]; then
+		git clone https://github.com/Zaedus/arc-dotfiles.git
+	fi
+
+	cd arc-dotfiles
+	nitrogen --set-zoom-fill arc-dark.png
+}
 
 install_dmenu() {
 	cd /tmp
@@ -43,7 +53,6 @@ install_dwm() {
 	fi
 
 	cd arc-dotfiles
-
 	sudo cp dwm.desktop /usr/share/xsessions/
 }
 
@@ -58,10 +67,10 @@ install_dotfiles() {
 backup_dotfiles() {
 	echo "This is going to overwrite your alacritty, dunst, gtk-2.0, gtk-3.0, and picom configs and the '~/.dwm' directory."
 	read -p "Are you sure you want to continue? [y/N] " -n 1 -r REPLY </dev/tty
+	echo
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		return 0
 	else
-		echo
 		exit
 	fi
 }
@@ -93,6 +102,8 @@ main() {
 	install_dwmblocks
 
 	install_dmenu
+
+	final_setup
 }
 
 main
